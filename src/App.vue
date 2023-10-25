7c9ac59062bd10bfc2c86b60ae075dcb489ffaa8
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2023-10-25 15:37:46 +0200

    mise en place de nom de fichier

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
